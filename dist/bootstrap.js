61e9dabce81e8405be59cac3899603a451aefe6e
Aluísio Rodrigues Amaral <aluisio@solucoesip.net>
2017-10-09 11:31:30 -0300

    Removed unintended cached files

--- dist/bootstrap.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const i18n = require("i18n");
-i18n.configure({
-    directory: __dirname + '/locales',
-    locales: ['en', 'br'],
-});
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./bootstrap");
-const RouterOSAPI_1 = require("./RouterOSAPI");
-module.exports = RouterOSAPI_1.RouterOSAPI;
