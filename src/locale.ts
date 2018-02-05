c49388e933b720aed3534ef4d9b7077f791ece53
Aluisio Amaral <aluisioramtl@gmail.com>
2018-02-05 17:06:46 -0200

    removed unneeded dependencies

--- src/locale.ts
+++ src/locale.ts
@@ -1,10 +1,7 @@
 import { normalize } from 'path';
 import * as i18n from 'i18next';
-import * as backend from 'i18next-sync-fs-backend';
 
-i18n
-    .use(backend)
-    .init({
+i18n.init({
         initImmediate: false,
         lng: 'en',
         fallbackLng: 'en',
