9802403e8c421c8b6ea73d05d8b349ec6cc11720
Aluisio Amaral <aluisioramtl@gmail.com>
2019-01-07 08:47:10 -0200

    fixed debounce call

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,8 @@
 export const debounce = (callback, timeout = 0) => {
     let timeoutObj = null;
-
-    return {
-        run: (...args) => {
-            const context = this;
-            clearTimeout(timeoutObj);
-            timeoutObj = setTimeout(() => callback.apply(context, args), timeout);
-        },
-
-        cancel: () => {
-            clearTimeout(timeoutObj);
-        },
+    return (...args) => {
+        const context = this;
+        clearTimeout(timeoutObj);
+        timeoutObj = setTimeout(() => callback.apply(context, args), timeout);
     };
 };
