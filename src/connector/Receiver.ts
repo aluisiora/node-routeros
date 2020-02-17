66344b2acf9a51298f456010df7941b86c6c3330
Zack Mattor <zmattor@gmail.com>
2020-02-16 20:37:25 -0600

    Fix for length descrptor getting cut by tcp transmission

--- src/connector/Receiver.ts
+++ src/connector/Receiver.ts
@@ -5,6 +5,7 @@ import { RosException } from '../RosException';
 
 const info = debug('routeros-api:connector:receiver:info');
 const error = debug('routeros-api:connector:receiver:error');
+const nullBuffer = new Buffer([0x00]);
 
 export interface ISentence {
     sentence: string;
@@ -74,10 +75,18 @@ export class Receiver {
     private currentPacket: string[] = [];
 
     /**
+     * Used to store a partial segment of the
+     * length descriptor if it gets split
+     * between tcp transmissions.
+     */
+    private lengthDescriptorSegment: Buffer;
+
+
+    /**
      * Receives the socket so we are able to read
      * the data sent to it, separating each tag
      * to the according listener.
-     * 
+     *
      * @param socket
      */
     constructor(socket: Socket) {
@@ -87,11 +96,11 @@ export class Receiver {
     /**
      * Register the tag as a reader so when
      * the routerboard respond to the command
-     * related to the tag, we know where to send 
+     * related to the tag, we know where to send
      * the data to
-     * 
-     * @param {string} tag 
-     * @param {function} callback 
+     *
+     * @param {string} tag
+     * @param {function} callback
      */
     public read(tag: string, callback: (packet: string[]) => void): void {
         info('Reader of %s tag is being set', tag);
@@ -106,8 +115,8 @@ export class Receiver {
      * from the tag mapping. Usually it is closed
      * after the command has being !done, since each command
      * opens a new auto-generated tag
-     * 
-     * @param {string} tag 
+     *
+     * @param {string} tag
      */
     public stop(tag: string): void {
         info('Not reading from %s tag anymore', tag);
@@ -118,38 +127,92 @@ export class Receiver {
      * Proccess the raw buffer data received from the routerboard,
      * decode using win1252 encoded string from the routerboard to
      * utf-8, so languages with accentuation works out of the box.
-     * 
+     *
      * After reading each sentence from the raw packet, sends it
      * to be parsed
-     * 
-     * @param {Buffer} data 
+     *
+     * @param {Buffer} data
      */
     public processRawData(data: Buffer): void {
+        if(this.lengthDescriptorSegment) {
+            data = Buffer.concat([this.lengthDescriptorSegment, data]);
+            this.lengthDescriptorSegment = null;
+        }
+
+        // Loop through the data we just received
         while (data.length > 0) {
+
+            // If this does not contain the beginning of a packet...
             if (this.dataLength > 0) {
+
+                // If the length of the data we have in our buffer
+                // is less than or equal to that reported by the
+                // bytes used to dermine length...
                 if (data.length <= this.dataLength) {
+
+                    // Subtract the data we are taking from the length we desire
                     this.dataLength -= data.length;
+
+                    // Add this data to our current line
                     this.currentLine += iconv.decode(data, 'win1252');
+
+                    // If there is no more desired data we want...
                     if (this.dataLength === 0) {
+
+                        // Push the data to the sentance
                         this.sentencePipe.push({
                             sentence: this.currentLine,
                             hadMore: (data.length !== this.dataLength)
                         });
+
+                        // process the sentance and clear the line
                         this.processSentence();
                         this.currentLine = '';
                     }
+
+                    // Break out of processRawData and wait for the next
+                    // set of data from the socket
                     break;
+
+                // If we have more data than we desire...
                 } else {
+                    // slice off the part that we desire
                     const tmpBuffer = data.slice(0, this.dataLength);
+
+                    // decode this segment
                     const tmpStr = iconv.decode(tmpBuffer, 'win1252');
+
+                    // Add this to our current line
                     this.currentLine += tmpStr;
+
+                    // save our line...
                     const line = this.currentLine;
+
+                    // clear the current line
                     this.currentLine = '';
+
+                    // cut off the line we just pulled out
                     data = data.slice(this.dataLength);
-                    const [index, length] = this.decodeLength(data);
+
+                    // determine the length of the next word. This method also
+                    // returns the number of bytes it took to describe the length
+                    const [descriptor_length, length] = this.decodeLength(data);
+
+                    // If we do not have enough data to determine
+                    // the length... we wait for the next loop
+                    // and store the length descriptor segment
+                    if(descriptor_length > data.length) {
+                        this.lengthDescriptorSegment = data;
+                    }
+
+                    // Save this as our next desired length
                     this.dataLength = length;
-                    data = data.slice(index); // get rid of excess buffer
-                    if (this.dataLength === 1 && data.equals(Buffer.from('\0', 'ascii'))) {
+
+                    // slice off the bytes used to describe the length
+                    data = data.slice(descriptor_length);
+
+                    // If we only desire one more and its the end of the sentance...
+                    if (this.dataLength === 1 && data.equals(nullBuffer)) {
                         this.dataLength = 0;
                         data = data.slice(1); // get rid of excess buffer
                     }
@@ -159,11 +222,20 @@ export class Receiver {
                     });
                     this.processSentence();
                 }
+
+            // This is the beginning of this packet...
+            // This ALWAYS gets run first
             } else {
-                const [index, length] = this.decodeLength(data);
+                // returns back the start index of the data and the length
+                const [descriptor_length, length] = this.decodeLength(data);
+
+                // store how long our data is
                 this.dataLength = length;
-                data = data.slice(index);
-                if (this.dataLength === 1 && data.equals(Buffer.from('\0', 'ascii'))) {
+
+                // slice off the bytes used to describe the length
+                data = data.slice(descriptor_length);
+
+                if (this.dataLength === 1 && data.equals(nullBuffer)) {
                     this.dataLength = 0;
                     data = data.slice(1); // get rid of excess buffer
                 }
@@ -173,11 +245,11 @@ export class Receiver {
 
     /**
      * Process each sentence from the data packet received.
-     * 
+     *
      * Detects the .tag of the packet, sending the data to the
      * related tag when another reply is detected or if
      * the packet had no more lines to be processed.
-     * 
+     *
      */
     private processSentence(): void {
         if (!this.processingSentencePipe) {
@@ -193,7 +265,7 @@ export class Receiver {
                         this.socket.emit('fatal');
                         return;
                     }
-    
+
                     info('Processing line %s', line.sentence);
 
                     if (/^\.tag=/.test(line.sentence)) {
@@ -220,14 +292,14 @@ export class Receiver {
                     } else {
                         process();
                     }
-                    
+
                 } else {
                     this.processingSentencePipe = false;
                 }
 
             };
 
-            process();            
+            process();
         }
     }
 
@@ -261,8 +333,8 @@ export class Receiver {
      *
      * Credits for George Joseph: https://github.com/gtjoseph
      * and for Brandon Myers: https://github.com/Trakkasure
-     * 
-     * @param {Buffer} data 
+     *
+     * @param {Buffer} data
      */
     private decodeLength(data: Buffer): number[] {
         let len;
